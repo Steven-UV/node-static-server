69a2545907d63d7ded09bb0b995e7f0fed0b06d4
Steven-UV <you@example.com>
2022-06-12 17:55:29 +0800

    匹配任意文件；处理不存在的文件；处理非 get 请求

--- index.ts
+++ index.ts
@@ -8,33 +8,37 @@ const server = http.createServer();
 const publicDir = p.resolve(__dirname, "public");
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
   const { method, url: path, headers } = request;
-  const { pathname, search } = url.parse(path as string);
-  switch (pathname) {
-    case "/index.html":
-      response.setHeader("Content-Type", "text/html; charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "index.html"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/style.css":
-      response.setHeader("Content-Type", "text/css; charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "style.css"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    case "/main.js":
-      response.setHeader("Content-Type", "text/javascript; charset=utf-8");
-      fs.readFile(p.resolve(publicDir, "main.js"), (error, data) => {
-        if (error) throw error;
-        response.end(data.toString());
-      });
-      break;
-    default:
-      response.statusCode = 404;
-      response.end();
+  const pathname = url.parse(path as string).pathname || "/index.html";
+
+  if (method !=='GET'){
+    response.statusCode = 405
+    response.end()
+    return
   }
+
+  let filename = pathname.substring(1);
+  if (filename === "") {
+    filename = "index.html";
+  }
+  fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+    if (error) {
+      if (error.errno === -4058) {
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, "404.html"), (error, data) => {
+          response.end(data);
+        });
+      }
+      if (error.errno === -4068) {
+        response.statusCode = 403;
+        response.end("无权查看目录内容");
+      } else {
+        response.statusCode = 500;
+        response.end("服务器繁忙，请稍后再试");
+      }
+    } else {
+      response.end(data);
+    }
+  });
 });
 
 server.listen(8888);
